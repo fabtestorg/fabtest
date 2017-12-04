1431fe659529b296b57b7e16bed938eeb97d4c2a
zouyunfeng <zyf7862634@163.com>
2017-12-04 16:09:00 +0800

    feat - change remove node

--- scripts/removenode.py
+++ scripts/removenode.py
@@ -1,13 +1,14 @@
 import sys
-from fabric.api import run
+from fabric.api import run, settings
 
 reload(sys)
 sys.setdefaultencoding('utf8')
 
 def remove_node(type):
-    if type == "all":
-        run("docker ps -a | awk '{print $1}' | xargs docker rm -f")
-    else:
-        run("docker ps -a | grep %s | awk '{print $1}' | xargs docker rm -f"%type)
+    with settings(warn_only=True):
+        if type == "all":
+            run("docker ps | awk '{print $1}' | xargs docker rm -f")
+        else:
+            run("docker ps | grep %s | awk '{print $1}' | xargs docker rm -f"%type)
 
 
