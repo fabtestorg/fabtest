584dc5a38d7646c387985770ba24387478c73a04
zouyunfeng <zyf7862634@163.com>
2017-12-05 18:44:01 +0800

    fix

--- scripts/event_analyse.py
+++ scripts/event_analyse.py
@@ -55,7 +55,7 @@ def scan_files(directory, prefix=None, postfix=None):
 	return files_list
 
 def analyse(config_dir, log_dir):
-	directory = "%s/event_logs/%s"%(config_dir,log_dir)
+	directory = "%sevent_logs/%s"%(config_dir,log_dir)
 	postfix = ".log"
 	print "扫描目录为:", directory
 	print "后缀名为:", postfix
