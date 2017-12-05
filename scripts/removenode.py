7dca7d18627cdb5adac8e588e0a0d241eca7206c
zouyunfeng <zyf7862634@163.com>
2017-12-05 09:38:24 +0800

    feat - change remove node

--- scripts/removenode.py
+++ scripts/removenode.py
@@ -7,8 +7,14 @@ sys.setdefaultencoding('utf8')
 def remove_node(type):
     with settings(warn_only=True):
         if type == "all":
-            run("docker ps -a | awk '{print $1}' | xargs docker rm -f")
+            run("docker rm -f $(docker ps -aq)")
+            run("docker images |grep 'dev\-peer'|awk '{print $3}'|xargs docker rmi -f")
         else:
             run("docker ps -a | grep %s | awk '{print $1}' | xargs docker rm -f"%type)
+            if type == "peer":
+                run("docker images |grep 'dev\-peer'|awk '{print $3}'|xargs docker rmi -f")
+
+        run("rm -rf ~/fabtest")
+        run("rm -rf ~/fabTestData")
 
 
