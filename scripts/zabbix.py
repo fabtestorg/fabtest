f681f88f8ab34295da82332ab698abee6f530f03
Zhiyuan Feng <fengzhiyuan@peersafe.cn>
2017-12-05 15:10:57 +0800

    fix - adjust zabbix start method

--- scripts/zabbix.py
+++ scripts/zabbix.py
@@ -7,7 +7,7 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 
 #cp zabbix config to remote
-def cp_zabbix_config(config_name, config_dir):
+def start_zabbix(config_name, config_dir):
     dir_name = "zabbix"
     with lcd(config_dir):
         local("tar -zcvf %s.tar.gz %s.conf" % (config_name, config_name))
@@ -15,11 +15,8 @@ def cp_zabbix_config(config_name, config_dir):
         # run("mkdir -p /etc/%s"%dir_name)/
         put("%s.tar.gz" %config_name, "/etc/%s/"%dir_name)
         local("rm %s.tar.gz" % config_name)
-    with cd("/etc/%s"%dir_name):
+    with cd("/etc/%s/"%dir_name):
         run("tar zxvfm %s.tar.gz" %config_name)
-        run("rm %s.tar.gz" %config_name)
+        # run("rm %s.tar.gz" %config_name)
         run("mv %s.conf zabbix-agent.conf"%config_name)
-
-#start remote zabbix
-def start_zabbix():
-    run("systemctl restart zabbix-agent")
+        run("systemctl restart zabbix-agent")
