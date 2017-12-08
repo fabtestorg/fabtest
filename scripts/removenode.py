60887710e174b3d39be9bf2b9dbfa8deb3397d01
zouyunfeng <zyf7862634@163.com>
2017-12-08 13:13:53 +0800

    commit

--- scripts/removenode.py
+++ scripts/removenode.py
@@ -13,12 +13,13 @@ def remove_node(type):
             run("docker ps -a | grep %s | awk '{print $1}' | xargs docker rm -f"%type)
             if type == "peer":
                 run("docker images |grep 'dev\-peer'|awk '{print $3}'|xargs docker rmi -f")
-
+        run("docker network prune -f")
         run("rm -rf ~/fabtest")
 
 def remove_client():
     with settings(warn_only=True):
         run("docker ps -a | awk '{print $1}' | xargs docker rm -f")
+        run("docker network prune -f")
         run("rm -rf ~/fabtest")
         utils.kill_process("eventserver")
 
