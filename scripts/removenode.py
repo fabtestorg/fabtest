57f441299be174f03b8dd0765d8baf92916aef3d
zouyunfeng <zyf7862634@163.com>
2017-12-08 13:01:12 +0800

    fix

--- scripts/removenode.py
+++ scripts/removenode.py
@@ -7,7 +7,7 @@ sys.setdefaultencoding('utf8')
 def remove_node(type):
     with settings(warn_only=True):
         if type == "all":
-            run("docker rm -f $(docker ps -aq)")
+            run("docker ps -a | awk '{print $1}' | xargs docker rm -f")
             run("docker images |grep 'dev\-peer'|awk '{print $3}'|xargs docker rmi -f")
         else:
             run("docker ps -a | grep %s | awk '{print $1}' | xargs docker rm -f"%type)
@@ -18,7 +18,7 @@ def remove_node(type):
 
 def remove_client():
     with settings(warn_only=True):
-        run("docker rm -f $(docker ps -aq)")
+        run("docker ps -a | awk '{print $1}' | xargs docker rm -f")
         run("rm -rf ~/fabtest")
         utils.kill_process("eventserver")
 
