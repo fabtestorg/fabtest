82e6e19ae1786264214d032e063bab994bdb4b7e
zyf <zyf7862634@163.com>
2018-07-24 19:22:43 +0800

    aa

--- scripts/event_analyse.py
+++ scripts/event_analyse.py
@@ -19,7 +19,7 @@ def loaddata(file):
 	return results
 
 def statistics(filename, results):
-	less1 = 0 # 小于1s的
+	less1_5 = 0 # 小于1s的
 	less3 = 0 # 小于3s的
 	more3 = 0 # 大于3s的
 	more8 = 0 # 大于8s的
@@ -27,8 +27,8 @@ def statistics(filename, results):
 	length = len(results)
 	for i in results:
 		total += i
-		if i < 1000:
-			less1+=1
+		if i < 1500:
+			less1_5+=1
 			less3+=1
 		elif i <= 3000:
 			less3+=1
@@ -37,7 +37,7 @@ def statistics(filename, results):
 			if i >= 8000:
 				more8+=1
 	if length > 0 :
-		print ("%s: aver:%.2f,max:%d,min:%d,total:%d    <3s:%d,%.2f    >3s:%d,%.2f    <1s:%d,%.2f    >8s:%d,%.2f"%(filename,float(total)/length,max(results),min(results),length,less3,float(less3)/length,more3,float(more3)/length,less1,float(less1)/length,more8,float(more8)/length))
+		print ("%s: aver:%.2f,max:%d,min:%d,total:%d    <3s:%d,%.2f    >3s:%d,%.2f    <1s:%d,%.2f    >8s:%d,%.2f"%(filename,float(total)/length,max(results),min(results),length,less3,float(less3)/length,more3,float(more3)/length,less1_5,float(less1_5)/length,more8,float(more8)/length))
 
 
 
