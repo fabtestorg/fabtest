e3048b229dcd2d6e903fd886e5aa6f54f34fc66a
zyf <zyf7862634@163.com>
2018-07-24 19:17:00 +0800

    feat -add sync  run chaincode

--- cmd/crypto.go
+++ cmd/crypto.go
@@ -3,8 +3,9 @@ package cmd
 import (
 	"fmt"
 	"github.com/fabtestorg/fabtest/tpl"
-	"time"
 	"strconv"
+	"sync"
+	"time"
 )
 
 func CreateCert() error {
@@ -20,14 +21,14 @@ func CreateYamlByJson(strType string) error {
 	if strType == "configtx" {
 		inputData := GetJsonMap("node.json")
 		orgcounts := inputData[OrgCounts].(float64)
-		var orgslist,kafkalist []string
+		var orgslist, kafkalist []string
 		for i := 1; i <= int(orgcounts); i++ {
-			orgslist = append(orgslist,strconv.Itoa(i))
+			orgslist = append(orgslist, strconv.Itoa(i))
 		}
-		kafkalist = append(kafkalist,"kafka0")
-		kafkalist = append(kafkalist,"kafka1")
-		kafkalist = append(kafkalist,"kafka2")
-		kafkalist = append(kafkalist,"kafka3")
+		kafkalist = append(kafkalist, "kafka0")
+		kafkalist = append(kafkalist, "kafka1")
+		kafkalist = append(kafkalist, "kafka2")
+		kafkalist = append(kafkalist, "kafka3")
 		inputData["orgs"] = orgslist
 		inputData["kafkas"] = kafkalist
 		inputData[PeerDomain] = inputData[PeerDomain].(string)
@@ -37,7 +38,7 @@ func CreateYamlByJson(strType string) error {
 		orgcounts := inputData[OrgCounts].(float64)
 		var orgslist []string
 		for i := 1; i <= int(orgcounts); i++ {
-			orgslist = append(orgslist,strconv.Itoa(i))
+			orgslist = append(orgslist, strconv.Itoa(i))
 		}
 		inputData["orgs"] = orgslist
 		return tpl.Handler(inputData, TplCryptoConfig, ConfigDir()+"crypto-config.yaml")
@@ -58,8 +59,8 @@ func CreateYamlByJson(strType string) error {
 			if strType == "client" {
 				if nodeType == TypePeer {
 					curorgid := value[OrgId].(string)
-					value[Order0_Address] = findMapValue(TypeOrder, "0", "1",IP)
-					value[Order1_Address] = findMapValue(TypeOrder, "1", curorgid,IP)
+					value[Order0_Address] = findMapValue(TypeOrder, "0", "1", IP)
+					value[Order1_Address] = findMapValue(TypeOrder, "1", curorgid, IP)
 					clientname := nodeType + value[PeerId].(string) + "org" + value[OrgId].(string)
 					//生成api docker-compose.yaml
 					err := tpl.Handler(value, TplApiDocker, ConfigDir()+clientname+"apidocker.yaml")
@@ -81,36 +82,36 @@ func CreateYamlByJson(strType string) error {
 			switch nodeType {
 			case TypeZookeeper:
 				curzkid := value[ZkId].(string)
-				value[IP0] = findMapValue(TypeZookeeper, "0", "",IP)
-				value[IP1] = findMapValue(TypeZookeeper, "1", "",IP)
-				value[IP2] = findMapValue(TypeZookeeper, "2", "",IP)
+				value[IP0] = findMapValue(TypeZookeeper, "0", "", IP)
+				value[IP1] = findMapValue(TypeZookeeper, "1", "", IP)
+				value[IP2] = findMapValue(TypeZookeeper, "2", "", IP)
 				yamlname = nodeType + curzkid
 				tplfile = TplZookeeper
 			case TypeKafka:
-				value[Zk_IP0] = findMapValue(TypeZookeeper, "0", "",IP)
-				value[Zk_IP1] = findMapValue(TypeZookeeper, "1", "",IP)
-				value[Zk_IP2] = findMapValue(TypeZookeeper, "2", "",IP)
+				value[Zk_IP0] = findMapValue(TypeZookeeper, "0", "", IP)
+				value[Zk_IP1] = findMapValue(TypeZookeeper, "1", "", IP)
+				value[Zk_IP2] = findMapValue(TypeZookeeper, "2", "", IP)
 				yamlname = nodeType + value[KfkId].(string)
 				tplfile = TplKafka
 			case TypeOrder:
-				value[KFK0_ADDRESS] = findMapValue(TypeKafka,"0","",IP)
-				value[KFK1_ADDRESS] = findMapValue(TypeKafka,"1","",IP)
-				value[KFK2_ADDRESS] = findMapValue(TypeKafka,"2","",IP)
-				value[KFK3_ADDRESS] = findMapValue(TypeKafka,"3","",IP)
+				value[KFK0_ADDRESS] = findMapValue(TypeKafka, "0", "", IP)
+				value[KFK1_ADDRESS] = findMapValue(TypeKafka, "1", "", IP)
+				value[KFK2_ADDRESS] = findMapValue(TypeKafka, "2", "", IP)
+				value[KFK3_ADDRESS] = findMapValue(TypeKafka, "3", "", IP)
 				yamlname = nodeType + value[OrderId].(string) + "ord" + value[OrgId].(string)
 				tplfile = TplOrderer
 			case TypePeer:
 				curid := value[PeerId].(string)
 				curorgid := value[OrgId].(string)
-				value[Order0_Address] = findMapValue(TypeOrder, "0", "1",IP)
-				value[Order1_Address] = findMapValue(TypeOrder, "1", curorgid,IP)
+				value[Order0_Address] = findMapValue(TypeOrder, "0", "1", IP)
+				value[Order1_Address] = findMapValue(TypeOrder, "1", curorgid, IP)
 				value[USECOUCHDB] = inputData[USECOUCHDB].(string)
 				if curid == "0" {
-					value[Other_PeerAddress] = findMapValue(TypePeer, "1", curorgid,IP)
+					value[Other_PeerAddress] = findMapValue(TypePeer, "1", curorgid, IP)
 				} else if curid == "1" {
-					value[Other_PeerAddress] = findMapValue(TypePeer, "0", curorgid,IP)
+					value[Other_PeerAddress] = findMapValue(TypePeer, "0", curorgid, IP)
 				} else if curid == "2" {
-					value[Other_PeerAddress] = findMapValue(TypePeer, "0", curorgid,IP)
+					value[Other_PeerAddress] = findMapValue(TypePeer, "0", curorgid, IP)
 				}
 				yamlname = nodeType + curid + "org" + curorgid
 				tplfile = TplPeer
@@ -194,21 +195,29 @@ func InstallChaincode(ccoutpath string) error {
 	inputData := GetJsonMap("node.json")
 	peerdomain := inputData[PeerDomain].(string)
 	list := inputData[List].([]interface{})
+	var wg sync.WaitGroup
+	wg.Add(len(list))
 	for _, param := range list {
 		value := param.(map[string]interface{})
 		value[PeerDomain] = peerdomain
 		nodeType := value[NodeType].(string)
 		if nodeType == TypePeer {
-			obj := NewFabCmd("chaincode.py", "")
 			orgid := value[OrgId].(string)
 			peerid := value[PeerId].(string)
 			peer_address := "peer" + peerid + ".org" + orgid + "." + peerdomain + ":7051"
-			err := obj.RunShow("install_chaincode", BinPath(), ConfigDir(), peer_address, peerid, orgid, peerdomain, ccoutpath)
-			if err != nil {
-				return err
-			}
+			go func(binPath, configDir, peerAds, PeerId, OrgId, Pdn, CCoutPath string) {
+				obj := NewFabCmd("chaincode.py", "")
+				err := obj.RunShow("install_chaincode", binPath, configDir, peerAds, PeerId, OrgId, Pdn, CCoutPath)
+				if err != nil {
+					fmt.Printf(err.Error())
+				}
+				wg.Done()
+			}(BinPath(), ConfigDir(), peer_address, peerid, orgid, peerdomain, ccoutpath)
+		} else {
+			wg.Done()
 		}
 	}
+	wg.Wait()
 	return nil
 }
 
@@ -216,6 +225,8 @@ func RunChaincode(ccname, channelName string) error {
 	inputData := GetJsonMap("node.json")
 	peerdomain := inputData[PeerDomain].(string)
 	list := inputData[List].([]interface{})
+	var wg sync.WaitGroup
+	wg.Add(len(list))
 	for _, param := range list {
 		value := param.(map[string]interface{})
 		value[PeerDomain] = peerdomain
@@ -239,23 +250,30 @@ func RunChaincode(ccname, channelName string) error {
 				"\\,'Org51MSP.member'\\,'Org52MSP.member'\\,'Org53MSP.member'\\,'Org54MSP.member'\\,'Org55MSP.member'" +
 				"\\,'Org56MSP.member'\\,'Org57MSP.member'\\,'Org58MSP.member'\\,'Org59MSP.member'\\,'Org60MSP.member'" +
 				"\\,'Org61MSP.member'\\,'Org62MSP.member'\\,'Org63MSP.member'\\,'Org64MSP.member'\\,'Org65MSP.member'" +
-					")\""
+				")\""
 			if orgid == "1" && peerid == "0" {
 				err := obj.RunShow("instantiate_chaincode", BinPath(), ConfigDir(), peer_address, peerid, orgid, peerdomain, channelName, ccname, initparam, policy)
 				if err != nil {
 					return err
 				}
-				time.Sleep(1 * time.Second)
+				time.Sleep(2 * time.Second)
+				wg.Done()
 			} else {
 				//txargs := `'{"Args":["query"\,"a"]}'`
-				txargs := `'{"Args":["DslQuery"\,"trackid"\,"{\"dslSyntax\":\"{\\\"selector\\\":{\\\"sender\\\":\\\"zhengfu0\\\"}}\"}"]}'`
-				err := obj.RunShow("test_query_tx", BinPath(), ConfigDir(), peer_address, peerid, orgid, peerdomain, channelName, ccname, txargs)
-				if err != nil {
-					return err
-				}
+				go func(peerads, Pid, OrgId string) {
+					txargs := `'{"Args":["DslQuery"\,"trackid"\,"{\"dslSyntax\":\"{\\\"selector\\\":{\\\"sender\\\":\\\"zhengfu0\\\"}}\"}"]}'`
+					err := obj.RunShow("test_query_tx", BinPath(), ConfigDir(), peerads, Pid, OrgId, peerdomain, channelName, ccname, txargs)
+					if err != nil {
+						fmt.Println(err)
+					}
+					wg.Done()
+				}(peer_address, peerid, orgid)
 			}
+		}else {
+			wg.Done()
 		}
 	}
+	wg.Wait()
 	return nil
 }
 
@@ -295,12 +313,12 @@ func findMapValue(findType, findid, findorgid, key string) string {
 			switch findType {
 			case TypeZookeeper:
 				zkid := value[ZkId].(string)
-				if zkid == findid{
+				if zkid == findid {
 					return value[IP].(string)
 				}
 			case TypeKafka:
 				kfkid := value[KfkId].(string)
-				if kfkid == findid{
+				if kfkid == findid {
 					return value[IP].(string)
 				}
 			case TypeOrder:
