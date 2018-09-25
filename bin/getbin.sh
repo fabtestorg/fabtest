90a21495cec083e9db9e788aad97a45f85c38b01
zyf <zyf7862634@163.com>
2018-09-25 11:27:55 +0800

    modify bin

--- bin/getbin.sh
+++ bin/getbin.sh
@@ -1,25 +1,7 @@
 #!/usr/bin/env bash
 
 set -x
-
-cd /opt/gopath/src/github.com/hyperledger/fabric/common/tools/cryptogen
-go build
-cd -
-
-cd /opt/gopath/src/github.com/hyperledger/fabric/common/tools/configtxgen
-go build
-cd -
-
-#cd /opt/gopath/src/github.com/hyperledger/fabric/common/tools/configtxlator
-#go build
-#cd -
-
-cd /opt/gopath/src/github.com/hyperledger/fabric/peer
-go build
-cd -
-
-mv /opt/gopath/src/github.com/hyperledger/fabric/peer/peer .
-mv /opt/gopath/src/github.com/hyperledger/fabric/common/tools/cryptogen/cryptogen . 
-mv /opt/gopath/src/github.com/hyperledger/fabric/common/tools/configtxgen/configtxgen . 
-#mv /opt/gopath/src/github.com/hyperledger/fabric/common/tools/configtxlator/configtxlator . 
-cp /opt/gopath/src/github.com/hyperledger/fabric/sampleconfig/core.yaml .
+cp /opt/gopath/src/github.com/hyperledger/fabric/build/docker/bin/peer .
+cp /opt/gopath/src/github.com/hyperledger/fabric/build/docker/bin/cryptogen .
+cp /opt/gopath/src/github.com/hyperledger/fabric/build/docker/bin/configtxgen .
+cp /opt/gopath/src/github.com/peersafe/gm-crypto/usr/bin/LICENSE .
